49d3952b5bf162b15acdcf011413940c8c659447
Ritik Raj <ritikraj2699@outlook.com>
2023-08-20 17:30:39 +0530

    added search feature

--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
-from . models import Room
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.db.models import Q
+from . models import Room, Topic
 from . forms import RoomForm
 
 # rooms = [
@@ -9,10 +12,32 @@ from . forms import RoomForm
 #     {'id':4, 'name':'Lets deploy'},
 # ]
 
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request,'User does not exists')
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms':rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q)|
+        Q(name__icontains=q)|
+        Q(description__icontains=q)
+    )
+
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+
+    context = {'rooms':rooms, 'topics':topics, 'room_count':room_count}
     return render(request,'base/home.html', context)
 
 def room(request, pk):
